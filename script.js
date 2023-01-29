c63bdc877fdc6e7c33811845bf3a20a3fe4dc448
SantiUlla <santiagoulla.di@gmail.com>
2023-01-29 15:47:45 -0300

    Add buttons event listeners

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
+// counters
+
 let humanCounter = 0;
 let computerCounter = 0;
 
-while (humanCounter < 5 && computerCounter < 5) {
-  let humanSelection = window.prompt("To five rounds:\nRock, Paper o Scisors?");
-  console.clear();
-  console.log("You chose " + humanSelection);
+// loop
 
+let humanSelection;
+
+function startGame(e) {
+  humanSelection = this.value;
+
+  // computer selection
   let computerSelection = "";
 
-  function getComputerSelection() {
-    randomChoice = Math.floor(Math.random() * 3);
+  randomChoice = Math.floor(Math.random() * 3);
 
-    computerSelection =
-      randomChoice === 0 ? "rock" : randomChoice === 1 ? "paper" : "scisors";
-    console.log("Computer chose " + computerSelection);
-  }
+  computerSelection =
+    randomChoice === 0 ? "rock" : randomChoice === 1 ? "paper" : "scisors";
 
-  getComputerSelection();
+  //
+
   console.log("");
 
   if (
-    humanSelection !== "rock" &&
-    humanSelection !== "paper" &&
-    humanSelection !== "scisors"
+    (humanSelection === "rock" && computerSelection === "scisors") ||
+    (humanSelection === "scisors" && computerSelection === "paper") ||
+    (humanSelection === "paper" && computerSelection === "rock")
   ) {
-    alert("that's not the game");
+    console.log("You Win");
+    humanCounter++;
+  } else if (humanSelection === computerSelection) {
+    console.log("It's a tie");
   } else {
-    if (
-      (humanSelection === "rock" && computerSelection === "scisors") ||
-      (humanSelection === "scisors" && computerSelection === "paper") ||
-      (humanSelection === "paper" && computerSelection === "rock")
-    ) {
-      console.log("You Win");
-      humanCounter++;
-    } else if (humanSelection === computerSelection) {
-      console.log("It's a tie");
-    } else {
-      console.log("You Lose");
-      computerCounter++;
-    }
+    console.log("You Lose");
+    computerCounter++;
   }
+
+  // results
+
+  console.log("You chose " + humanSelection);
+
+  console.log("Computer chose " + computerSelection);
   console.log("");
   console.log("You " + humanCounter);
   console.log("CPU " + computerCounter);
+
+  if (humanCounter === 5) {
+    console.log("You Win!");
+  } else {
+    console.log("You Lose!");
+  }
 }
 
-if (humanCounter === 5) {
-  alert("You Win!");
-} else {
-  alert("You Lose!");
-}
-
-// console.log(humanSelection === "rock");
+// Link buttons
+const buttons = document.querySelectorAll("button");
+buttons.forEach((btn) => btn.addEventListener("click", startGame));
